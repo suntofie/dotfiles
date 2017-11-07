20b523ff4a1855822368731110f9144c0e96c9fc
suntofie <sparrkywells@gmail.com>
2017-11-07 23:11:54 +0300

    november update

---
+++ sh/aliases.sh
@@ -0,0 +1,21 @@
+  alias pi="sudo pacman -S "
+  alias yi="yaourt -S "
+  alias del="sudo pacman -Rs"
+  alias pup="sudo pacman -Syyu"
+  alias yup="yaourt -Syyu && yaourt -Syu && yaourt -Syu --aur"
+
+  alias s="sudo "
+  alias shut="shutdown now"
+
+  alias rex="xrdb -merge ~/.Xresources"
+  alias sx="startx"
+  alias xs="startx"
+
+  alias cd..='cd ..'
+  alias .='cd ..'
+  alias ..='cd ../..'
+  alias ~='cd ~'
+
+  alias ls="els --els-icons=fontawesome"
+
+  alias rebash="source ~/.bashrc"
---
+++ symlinks.sh
@@ -0,0 +1,60 @@
+#!/bin/bash 
+
+echo -n "Create symlinks? (Y/n) "
+
+read item
+case "$item" in
+    y|Y) echo "Creating symlinks..."
+        ;;
+    n|N) echo "exit from script"
+        exit 0
+        ;;
+    *) echo "They did not enter anything. Perform the default action..."
+        ;;
+esac
+echo "termite [1/6]" && sleep 1
+#ln -s ~/bin/dotfiles/termite ~/.config/termite
+echo "bspwm [2/6]" && sleep 1
+#ln -s ~/bin/dotfiles/bspwm ~/.config/bspwm
+echo "sxhkd [3/6]" && sleep 1
+#ln -s ~/bin/dotfiles/sxhkd ~/.config/sxhkd
+echo "polybar [4/6]" && sleep 1
+#ln -s ~/bin/dotfiles/polybar ~/.config/polybar
+echo "neovim [5/6]" && sleep 1
+#ln -s ~/bin/dotfiles/nvim ~/.config/nvim
+echo "sh [6/6]" && sleep 1
+#ln -s ~/bin/dotfiles/sh ~/bin/sh
+
+echo -n "Add sh scripts to bashrc? (Y/n) "
+
+read item
+case "$item" in
+    y|Y) echo "Addition..."
+        echo "source ~/bin/sh/aliases.sh" >> ~/.bashrc
+        echo "source ~/bin/sh/ex.sh" >> ~/.bashrc
+        ;;
+    n|N) echo "Skiping..." 
+        ;;
+    *) echo "They did not enter anything. Perform the default action..."
+	     echo "source ~/bin/sh/aliases.sh" >> ~/.zshrc
+        echo "source ~/bin/sh/ex.sh" >> ~/.zshrc
+		  ;;
+esac
+
+echo -n "Add sh scripts to zshrc? (Y/n) "
+
+read item
+case "$item" in
+    y|Y) echo "Addition..."
+        echo "source ~/bin/sh/aliases.sh" >> ~/.zshrc
+        echo "source ~/bin/sh/ex.sh" >> ~/.zshrc
+        ;;
+    n|N) echo "Skiping..." 
+        ;;
+    *) echo "They did not enter anything. Perform the default action..."
+        echo "source ~/bin/sh/aliases.sh" >> ~/.zshrc
+        echo "source ~/bin/sh/ex.sh" >> ~/.zshrc
+        ;;
+esac
+echo "" && sleep 1
+echo "done"
